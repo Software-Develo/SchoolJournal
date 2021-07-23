8e1ab403de5a164f3c31f0e53748483ddd956431
Software-Develo <khisamutdinovmarat@gmail.com>
2021-07-23 18:17:23 +0500

    4.Change main.cpp

--- SchoolJournalMain/main.cpp
+++ SchoolJournalMain/main.cpp
@@ -4,7 +4,7 @@
 //
 //  Created by Marat Khisamutdinov on 7/22/21.
 //
-//
+// /Users/maratkhisamutdinov/Desktop/spisok.txt
 
 #include <iostream>
 #include <string.h>
@@ -24,24 +24,43 @@ const char *sign = " \t \r \n";
 class People{
 public:
     string fullname;
-    vector<vector<string>> vect;
+    vector<vector<string>> vect_peo;
 };
 class Diary{
+    int i = 0;
+    int j = 0;
 public:
     vector<People> vect_dia;
+
+    void print(){// Выводим вектор на экран
+        for(int a = 0; a < vect_dia.size(); a++){
+            cout << endl << "Ученик" << "\t" << vect_dia[a].fullname << endl;
+            while(i < vect_dia[a].vect_peo.size()) {
+                cout << "Предмет" << "\t";
+                while (j < vect_dia[a].vect_peo[i].size()) {
+                    cout << vect_dia[a].vect_peo[i][j] << endl;
+                    j++;
+                }
+                j = 0;
+                i++;
+            }
+            i = 0;
+        }
+    }
 };
 
 int main(int argc, const char * argv[]) {
     setlocale(LC_ALL, "rus");
 
     string st_path;
-    string stud_name;
-    string lexem;
+    string name;
     string subject;
     char c_line[100];
     char *ptr;
     int stud_enter = 0;
     int subj_enter = 0;
+    bool ItIsEnd = false;
+    bool enter = false;
 
     cout << "Дневник" << endl << "Введите путь до файла" << endl << "Путь: ";
     cin >> st_path;
@@ -54,40 +73,63 @@ int main(int argc, const char * argv[]) {
     st_fail.open(st_path);
     if(st_fail.is_open()){
         cout << "Файл " << st_path << " открылся";
-
+        vector<string> vect;
         st_fail.getline(c_line, 100);
         while(!st_fail.eof()){
             ptr = strtok(c_line, sign);
-            lexem = string(ptr);
-            if(lexem == "Ученик"){
+            if(string(ptr) == "Ученик"){
+                if(ItIsEnd){
+                    diary.vect_dia.push_back(people);
+                    people.vect_peo.clear();
+                }
                 while((ptr = strtok(NULL, sign)) != NULL){
                     stud_enter++;
                     if(stud_enter == 1){
-                        stud_name = lexem;
+                        name = string(ptr);
                     }else{
-                        stud_name.push_back(' ');
-                        stud_name.append(lexem);
+                        name.push_back(' ');
+                        name.append(string(ptr));
                     }
                 }
+                people.fullname = name;
                 stud_enter = 0;
-            }else if(lexem == "Предмет"){
+            }else if(string(ptr) == "Предмет"){
+                subj_enter = 0;
                 while((ptr = strtok(NULL, sign)) != NULL){
-                    if(lexem.find_first_of(letters) != string::npos){// Если в лексеме есть хоть одна буква
+                    if(string(ptr).find_first_of(letters) != string::npos){// Если в лексеме есть хоть одна буква
                         subj_enter++;
                         if(subj_enter == 1){
-                            subject = lexem;
+                            subject = string(ptr);
                         }else{
                             subject.push_back(' ');
-                            subject.append(lexem);
+                            subject.append(string(ptr));
                         }
-                    }else if(lexem.find_first_of(letters) == string::npos && lexem.find_first_of(symbol) == string::npos && lexem.find_first_of(num) != string::npos){
-
+                        enter = true;
+                    }
+                    // Если нет букв, символов, но есть цифры
+                    else if(string(ptr).find_first_of(letters) == string::npos && string(ptr).find_first_of(symbol) == string::npos && string(ptr).find_first_of(num) != string::npos){
+                        if(enter) {subj_enter = 0; enter = false;}
+                        subj_enter++;
+                        if(subj_enter == 1){
+                            vect.push_back(subject);
+                            vect.push_back(string(ptr));
+                        }else{
+                            vect.push_back(string(ptr));
+                        }
                     }
                 }
+                people.vect_peo.push_back(vect);
+                vect.clear();
+                ItIsEnd = true;
             }
+            st_fail.getline(c_line, 100);
         }
-    }else cout << "Файл " << st_path << " не открылся!!!!!";
+        diary.vect_dia.push_back(people);
+        people.vect_peo.clear();
+        diary.print();
+    }else cout << "Файл " << st_path << " не открылся!!!!!" << endl;
 
+    cout << "Конец!!!" << endl;
     st_fail.close();
     return 0;
 
